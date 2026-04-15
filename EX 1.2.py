53b38fc529fdecf49cd67213040bab3dcdb4b834
Nguyenvanhieu738 <25112044@st.vju.ac.vn>
2026-04-15 21:27:32 +0700

    Add files via upload

---
+++ EX 1.2.py
@@ -0,0 +1,22 @@
+## câu 1.2
+minutes = 42   #câu 1 
+seconds = 42 
+seconds = minutes*60 + seconds
+print("so giay la: ",seconds)
+
+kilometers = 10      # câu 2
+miles = kilometers/1.61
+print("miles= ", miles)
+
+hours = seconds/3600    #câu 3
+pace_hours = miles/hours
+pace_mninutes = minutes/miles
+pace_seconds = seconds/miles
+
+print("pace/hours = ", pace_hours)
+print("pace/minutes = ", pace_mninutes)
+print("pace/seconds = ", pace_seconds)
+
+
+
+
